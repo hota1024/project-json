632a63cfd18d22227f25300d3b6d69a6ccba67d7
hota1024 <hotalog@hota1024.com>
2020-01-19 13:33:01 +0900

    :fire: tests/hello.test.ts

--- tests/hello.test.ts
+++
@@ -1,8 +0,0 @@
-import hello from '../src'
-
-describe('hello test', () => {
-  it('hello should return message string.', () => {
-    expect(hello('hoge')).toBe('Hello hoge!')
-    expect(hello('hoge', '.')).toBe('Hello hoge.')
-  })
-})

ad1fd8fa64a0e6fc0898ebb2fc033256ff12103b
hota1024 <hotalog@hota1024.com>
2020-01-20 14:46:06 +0900

    :label: Add ProjectJson type

---
+++ src/types/ProjectJson/index.ts
@@ -0,0 +1,54 @@
+import { OPCode } from '../OPCode'
+
+export type ProjectJson = {
+  targets: {
+    isStage: true
+    name: string
+    variables: {
+      [id: string]: [string, any]
+    }
+    list: {
+      [id: string]: [string, any[]]
+    }
+    broadcasts: {
+      [id: string]: string
+    }
+    blocks: {
+      [id: string]: {
+        opcode: OPCode
+        next?: string
+        parent?: string
+        inputs: {
+          [name: string]: any[]
+        }
+        fields: {
+          [name: string]: any[]
+        }
+        shadow: boolean
+        topLevel: boolean
+        mutation?: {
+          tagName: string
+          children: []
+          proccode: string
+          argumentids: string
+          argumentnames: string
+          argumentdefaults: string
+          wrap: string
+        }
+        x?: number
+        y?: number
+      } | [
+        number,
+        string,
+        string,
+        number,
+        number
+      ]
+    }
+    monitors: {
+      id: string
+      mode: string
+      opcode: 
+    }[]
+  }[]
+}

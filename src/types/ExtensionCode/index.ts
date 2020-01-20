9a9e390f1e528a0eabf6543035895cf92306e8d6
hota1024 <hotalog@hota1024.com>
2020-01-20 16:06:35 +0900

    :label: Add ExtensionCode type

---
+++ src/types/ExtensionCode/index.ts
@@ -0,0 +1,13 @@
+export enum ExtensionCode {
+  Music = 'music',
+  Pen = 'pen',
+  VideoSensing = 'videoSensing',
+  Text2Speech = 'text2speech',
+  Translate = 'translate',
+  MakeyMakey = 'makeymakey',
+  MicroBit = 'microbit',
+  EV3 = 'ev3',
+  Boost = 'boost',
+  Wedo2 = 'wedo2',
+  GoDirectForceAndAcceleration = 'gdxfor'
+}

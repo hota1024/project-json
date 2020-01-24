87f0502050cbed9208a23db8cb3659a341b1275d
hota1024 <hotalog@hota1024.com>
2020-01-24 21:34:06 +0900

    :label: Add VariableBlock interface.

---
+++ src/interfaces/VariableBlock/index.ts
@@ -0,0 +1,11 @@
+import { DataBlockInterface } from '../DataBlock'
+
+/*
+ * VariableBlock interface.
+ */
+export interface VariableBlockInterface extends DataBlockInterface {
+  /**
+   * Variable ID.
+   */
+  variableId: string
+}

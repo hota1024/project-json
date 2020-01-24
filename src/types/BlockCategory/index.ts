af1f9891d0823db6b1ae2ae4eda348c3064270e8
hota1024 <hotalog@hota1024.com>
2020-01-24 21:32:48 +0900

    :label: Add BlockCategory type

---
+++ src/types/BlockCategory/index.ts
@@ -0,0 +1,26 @@
+/*
+ * BlockCategory.
+ */
+export enum BlockCategory {
+  Motion,
+  Looks,
+  Sound,
+  Event,
+  Control,
+  Sensing,
+  Operator,
+  Data,
+  Procedures,
+  Music,
+  Pen,
+  VideoSensing,
+  Text2speech,
+  Translate,
+  Makeymakey,
+  Microbit,
+  Ev3,
+  Boost,
+  Wedo2,
+  Gdxfor,
+  Unknown
+}
